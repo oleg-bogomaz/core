ef90d8943ee0ab5fa6ae9c6b7772369fa94ce670
Oleg Bogomaz <bogomaz@evrone.com>
2024-06-08 11:49:28 +0900

    add test and pull request check

---
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { CourseItem } from "./course-item"
+
+describe("course item", () => {
+    it("should call delete callback", async () => {
+        const onDelete = jest.fn()
+        render(
+            <CourseItem
+                course={{ id: "1", name: "name", description: "description" }}
+                onDelete={onDelete}
+            />,
+        )
+
+        await userEvent.click(screen.getByText("Удалить"))
+
+        expect(onDelete).toHaveBeenCalled()
+    })
+})
